fd7eeb87291c64a80f9e8a9a7ce2f1f839862c84
Raahuldas <153657088+Raahuldas@users.noreply.github.com>
2024-02-07 00:07:23 +0530

    Add files via upload

---
+++ scripts/bag.js
@@ -0,0 +1,103 @@
+const ConvenienceFees = 99;
+console.log(bagItems)
+let bagItemsObj = [];
+
+onload();
+
+function onload(){
+    loadBagItemObj();
+    displayBagItems();
+    displayBagSummary();
+}
+
+function removeFromBag(itemId) {
+    bagItems = bagItems.filter(bagItemsId=> bagItemsId != itemId)
+    localStorage.setItem("bagItems",JSON.stringify(bagItems));
+    loadBagItemObj();
+    displayBagItems();
+    displayBagSummary();
+}
+
+function loadBagItemObj(){
+    bagItemsObj = bagItems.map(itemId=>{
+        for (let i = 0; i < items.length; i++) {
+            if( itemId == items[i].id){
+                return items[i];
+            }
+        }
+    })
+    console.log(bagItemsObj)
+}
+
+function displayBagItems() {
+    let bagItemsContainer = document.querySelector('.bag-items-container');
+    let bagItemContent = ""    
+    bagItemsObj.forEach(bagItem => {
+        bagItemContent += generateItemHtml(bagItem);
+    });
+    bagItemsContainer.innerHTML=bagItemContent;
+}
+
+function generateItemHtml(item) {
+return `<div class="bag-item-container">
+        <div class="item-left-part">
+        <img class="bag-item-img" src="../${item.image}">
+        </div>
+        <div class="item-right-part">
+        <div class="company">${item.company}</div>
+            <div class="item-name">${item.item_name}</div>
+            <div class="price-container">
+                <span class="current-price">Rs ${item.current_price}</span>
+                <span class="original-price">Rs ${item.original_price}</span>
+                <span class="discount-percentage">(${item.discount_percentage}% OFF)</span>
+                </div>
+                <div class="return-period">
+                <span class="return-period-days">${item.return_period} days</span> return available
+                </div>
+                <div class="delivery-details">
+                Delivery by
+                <span class="delivery-details-days">${item.delivery_date}</span>
+                </div>
+                </div>
+                <div class="remove-from-cart" onclick="removeFromBag(${item.id})">X</div>
+                </div>
+                `;
+}
+
+function displayBagSummary(){
+    let totalItem = bagItems.length;
+    let totalMRP=0;
+    let totalDiscount = 0;
+    
+    bagItemsObj.forEach((bagItem)=>{
+        totalMRP += bagItem.original_price;
+        totalDiscount += bagItem.original_price - bagItem.current_price;
+    })
+    
+    let finalPayment = totalMRP-totalDiscount+ConvenienceFees;
+
+    let bagSummary = document.querySelector(".bag-summary");
+    bagSummary.innerHTML=`<div class="bag-details-container">
+    <div class="price-header">PRICE DETAILS (${totalItem} Items) </div>
+    <div class="price-item">
+        <span class="price-item-tag">Total MRP</span>
+        <span class="price-item-value">₹${totalMRP}</span>
+    </div>
+    <div class="price-item">
+        <span class="price-item-tag">Discount on MRP</span>
+        <span class="price-item-value priceDetail-base-discount">-₹${totalDiscount}</span>
+    </div>
+    <div class="price-item">
+        <span class="price-item-tag">Convenience Fee</span>
+        <span class="price-item-value">₹${ConvenienceFees}</span>
+    </div>
+    <hr>
+    <div class="price-footer">
+        <span class="price-item-tag">Total Amount</span>
+        <span class="price-item-value">₹${finalPayment}</span>
+    </div>
+</div>
+<button class="btn-place-order">
+    <div class="css-xjhrni">PLACE ORDER</div>
+</button>`
+}
---
+++ scripts/index.js
@@ -0,0 +1,56 @@
+let bagItems;
+let bagCount = document.querySelector('.bag-item-count');
+
+onload();
+
+function onload(){
+    let bagItemsVal = localStorage.getItem("bagItems");
+    bagItems = bagItemsVal? JSON.parse(bagItemsVal): [];
+    loadItemsOnHomePage();
+    displayBagCount();
+    displayBagCount();
+}
+
+
+function addToBag(itemId){
+bagItems.push(itemId);
+localStorage.setItem("bagItems",JSON.stringify(bagItems));
+displayBagCount();
+}
+
+function displayBagCount(){
+    if (bagItems.length>0) {
+        bagCount.innerText = bagItems.length;
+        bagCount.style.visibility="visible";
+    }else{
+        bagCount.style.visibility="hidden";
+    }
+}
+
+function loadItemsOnHomePage(){
+    let itemsContainer = document.querySelector('.items-container');
+    let contents="";
+    if (!itemsContainer) {
+        return;
+    }else{
+        items.forEach((item)=>{
+            contents += `
+            <div class="item-container">
+            <img class="item-image" src="${item.image}" alt="item image">
+            <div class="rating">
+            ${item.rating.stars} ⭐ | ${item.rating.count}
+            </div>
+            <div class="company-name">${item.company}</div>
+            <div class="item-name">${item.item_name}</div>
+            <div class="price">
+            <span class="current-price">Rs ${item.current_price}</span>
+            <span class="original-price">Rs ${item.original_price}</span>
+            <span class="discount">(${item.discount_percentage}% OFF)</span>
+            </div>
+            <button class="btn-add-bag" onclick="addToBag(${item.id})">Add to Bag</button>
+            </div>`;
+        })
+    }
+    
+    itemsContainer.innerHTML=contents;
+}
